1bec254fcbdd5bbc1d8b49db300414cebd9201cb
Valery Kvon <addagger@gmail.com>
2013-01-06 21:05:54 +0400

    version 0.0.2

--- lib/delegate_attributes/models/active_record_extension.rb
+++ lib/delegate_attributes/models/active_record_extension.rb
@@ -11,22 +11,25 @@ module DelegateAttributes
     
       def delegate_attributes(*args)
         options = args.extract_options!
+        errors_option = options.delete(:errors)
+				writer_option = options.delete(:writer)
+				
         writer_regexp = /=\z/
         readers = args.select {|a| a.to_s !=~ writer_regexp}
         writers = args.select {|a| a.to_s =~ writer_regexp}
-        if options[:writer] == true
+        if writer_option == true
           writers += readers.map {|a| "#{a}="}
         end
-        
+
         class_eval do
-          delegate *(readers + writers), :to => options[:to]
+          delegate *(readers + writers), options.dup
         end          
         
-        unless options[:errors] == false
+        unless errors_option == false
           class_eval <<-EOV
             validate do
               object = #{options[:to]}
-              #{"object.instance_variable_set(:@errors, DelegateAttributes::FakeErrors.new(object))" if options[:errors].to_s == "fit"}
+              #{"object.instance_variable_set(:@errors, DelegateAttributes::FakeErrors.new(object))" if errors_option.to_s == "fit"}
               if !object.valid?
                 object.errors.messages.each do |attribute, suberrors|
                   if attribute.to_s.in? %w{#{readers.join(" ")}}
