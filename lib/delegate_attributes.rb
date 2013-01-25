2b694872b7e3a700c9809b92162048af3b7363f8
Valery Kvon <addagger@gmail.com>
2013-01-25 22:40:35 +0400

    moved to "active_tools"

--- lib/delegate_attributes.rb
+++ lib/delegate_attributes.rb
@@ -4,6 +4,14 @@ module DelegateAttributes
   def self.load!
     require 'delegate_attributes/engine'
     require 'delegate_attributes/railtie'
+    warn <<-EOW
+
+    DEPRECATION WARNING: Gem 'delegate_attributes' has been moved into 'active_tools' gem.
+    Further development of 'delegate_attributes' will no longer continue.
+
+    Thank you!
+
+EOW
   end
 
 end
